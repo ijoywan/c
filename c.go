41a40a4bb1bad2aab591b0f74f81ebe173e63920
ijoywan <39815436+ijoywan@users.noreply.github.com>
2018-08-03 15:02:17 +0800

    Create c.go
    create module file  c.go

---
+++ c.go
@@ -0,0 +1,15 @@
+package c
+
+import (
+	"fmt"
+
+	"github.com/ijoywan/d"
+)
+
+func CallC() {
+	fmt.Println("call C: v1.0.0")
+	fmt.Println("   --> call D:")
+	fmt.Printf("\t")
+	d.CallD()
+	fmt.Println("   --> call D end")
+}
